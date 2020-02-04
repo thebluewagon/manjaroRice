a089568b3a2bebc4d9c706e647bf6427ce2dcabe
Brayden Alvarez <braydenalv@gmail.com>
2020-02-03 20:29:42 -0600

    added config files

---
+++ screenlayout/profile.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --primary --mode 2560x1440 --pos 1080x184 --rotate normal -r 143.91  --output DisplayPort-1 --off --output HDMI-A-0 --off --output HDMI-A-1 --mode 1920x1080 --pos 0x0 --rotate left
